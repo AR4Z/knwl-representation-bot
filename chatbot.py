dbe8b053d3b2290ddcf78e97a1c4ac48474d2c2d
Orlando Hernandez <oaraz02@gmail.com>
2019-09-16 15:48:45 -0500

    add feasible params

--- chatbot.py
+++ chatbot.py
@@ -1,19 +1,30 @@
 from chatterbot import ChatBot
+from chatterbot.comparisons import levenshtein_distance
+from chatterbot.response_selection import get_random_response, get_most_frequent_response
+from chatterbot.comparisons import levenshtein_distance
 from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.logic import BestMatch
 
+bot = ChatBot('Norman', 
+    logic_adapters=[
+        {
+            'import_path': 'chatterbot.logic.BestMatch',
+            "statement_comparison_function": levenshtein_distance,
+            "response_selection_method": get_most_frequent_response,
+            'maximum_similarity_threshold': 0.51,
+            'default_response': 'Disculpa, no te he entendido bien, mi único conocimiento es acerca de Representación del Conocimiento. ¿Puedes ser más específico?.'
+        }
+    ],
+    preprocessors=[
+        'chatterbot.preprocessors.clean_whitespace'
+    ],
+    read_only=True)
 
-bot = ChatBot('Norman')
-
-trainer = ListTrainer(bot)
-
-trainer.train([
-    'How are you?',
-    'I am good.',
-    'That is good to hear.',
-    'Thank you',
-    'You are welcome.',
-])
-
+trainer = ChatterBotCorpusTrainer(bot)
+trainer.train(
+   "./data/semantic-networks.yml"
+)
 
 while True:
     try:
